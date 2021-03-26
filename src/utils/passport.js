a01e454b387b8a22dcb7b3f9d8816ca051438a83
Tev <tevondavisjc@gmail.com>
2021-03-26 03:04:51 -0500

    jwtverify

---
+++ src/utils/passport.js
@@ -0,0 +1,26 @@
+const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
+const config = require("../config/");
+const User = require("../services/user.service");
+
+const jwtOptions = {
+  secretOrKey: config.jwt.secret,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+const jwtVerify = async (payload, done) => {
+  try {
+    const user = await User.getUserById(payload.sub);
+    if (!user) {
+      return done(null, false);
+    }
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
+};
+
+const jwtStrategy = new JwtStrategy(jwtOptions, jwtVerify);
+
+module.exports = {
+  jwtStrategy,
+};
