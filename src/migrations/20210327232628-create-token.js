e45a53bc8324f56474a05e40444e782d1b429ef6
Tev <tevondavisjc@gmail.com>
2021-03-29 19:55:10 -0500

    token migration

---
+++ src/migrations/20210327232628-create-token.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Tokens", {
+      token: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      blacklisted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      expires_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Tokens");
+  },
+};
