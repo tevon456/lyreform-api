c1562019619ffd2a4966a6cb219bd64b23923aa9
Tev <tevondavisjc@gmail.com>
2021-03-29 23:26:56 -0500

    modified migrations

--- src/migrations/20210327232628-create-token.js
+++ src/migrations/20210327232628-create-token.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Tokens", {
+    await queryInterface.createTable("tokens", {
       token: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -22,6 +22,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Tokens");
+    await queryInterface.dropTable("tokens");
   },
 };
