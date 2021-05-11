eb40b019bd46bca40942c4d1ad85019d350f0204
Tev <tevondavisjc@gmail.com>
2021-05-11 01:14:33 -0500

    create form model and association wit user

---
+++ src/models/form.js
@@ -0,0 +1,73 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Form extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+    // exclude fields from model when returning json
+    toJSON() {
+      return { ...this.get(), id: undefined };
+    }
+  }
+  Form.init(
+    {
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      logo_url: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      header_foreground: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      header_background: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      foreground: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      background: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      controls_foreground: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      controls_background: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      page_background: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      published: { type: DataTypes.BOOLEAN, defaultValue: true },
+      fields: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+      },
+    },
+    {
+      sequelize,
+      modelName: "Form",
+    }
+  );
+
+  return Form;
+};
--- src/models/relationships/index.js
+++ src/models/relationships/index.js
@@ -1,9 +1,17 @@
-function Relationships({ User, Token, ...rest }) {
+function Relationships({ User, Token, Form, ...rest }) {
+  // User Token Relationship
   User.hasMany(Token, {
     foreignKey: "user_id",
     onDelete: "CASCADE",
   });
   Token.belongsTo(User);
+
+  // User Form Relationship
+  User.hasMany(Form, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+  });
+  Form.belongsTo(User);
 }
 
 module.exports = Relationships;
