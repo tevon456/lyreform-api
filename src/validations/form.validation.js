667ee0119f5a196e98e384a6f90726a47a1c7929
Tev <tevondavisjc@gmail.com>
2021-05-14 01:52:32 -0500

    wip form validation

---
+++ src/validations/form.validation.js
@@ -0,0 +1,77 @@
+const Joi = require("@hapi/joi");
+const { color } = require("./custom.validation");
+
+const createForm = {
+  body: Joi.object().keys({
+    name: Joi.string().max(128).min(3).required(),
+    logo_url: Joi.string(),
+    published: Joi.boolean().required(),
+    header_foreground: Joi.string().required().custom(color),
+    header_background: Joi.string().required().custom(color),
+    body_foreground: Joi.string().required().custom(color),
+    body_background: Joi.string().required().custom(color),
+    controls_foreground: Joi.string().required().custom(color),
+    controls_background: Joi.string().required().custom(color),
+    page_background: Joi.string().required().custom(color),
+    fields: Joi.array()
+      .required()
+      .items({
+        id: Joi.string().required(),
+        name: Joi.string().required(),
+        label: Joi.string().required(),
+        field_type: Joi.string().required(),
+        placeholder: Joi.string(),
+        min: Joi.number(),
+        max: Joi.number(),
+        required: Joi.boolean().required(),
+        options: Joi.array().items({
+          value: Joi.alternatives().try(Joi.string(), Joi.array()),
+        }),
+      }),
+  }),
+};
+
+const updateForm = {
+  params: Joi.object().keys({
+    formId: Joi.string().required(),
+  }),
+  body: Joi.object().keys({
+    name: Joi.string().max(128).min(3).required(),
+    logo_url: Joi.string(),
+    published: Joi.boolean().required(),
+    header_foreground: Joi.string().required().custom(color),
+    header_background: Joi.string().required().custom(color),
+    body_foreground: Joi.string().required().custom(color),
+    body_background: Joi.string().required().custom(color),
+    controls_foreground: Joi.string().required().custom(color),
+    controls_background: Joi.string().required().custom(color),
+    page_background: Joi.string().required().custom(color),
+    fields: Joi.array()
+      .required()
+      .items({
+        id: Joi.string().required(),
+        name: Joi.string().required(),
+        label: Joi.string().required(),
+        field_type: Joi.string().required(),
+        placeholder: Joi.string(),
+        min: Joi.number(),
+        max: Joi.number(),
+        required: Joi.boolean().required(),
+        options: Joi.array().items({
+          value: Joi.alternatives().try(Joi.string(), Joi.array()),
+        }),
+      }),
+  }),
+};
+
+const deleteForm = {
+  params: Joi.object().keys({
+    formId: Joi.string().required(),
+  }),
+};
+
+module.exports = {
+  createForm,
+  updateForm,
+  deleteForm,
+};
