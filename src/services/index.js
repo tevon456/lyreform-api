4f3f089af2dd67faa5898746810598c125bd42b8
Tev <tevondavisjc@gmail.com>
2021-05-14 23:30:49 -0500

    export form service

--- src/services/index.js
+++ src/services/index.js
@@ -2,5 +2,5 @@ module.exports.authService = require("./auth.service");
 module.exports.emailService = require("./email.service");
 module.exports.tokenService = require("./token.service");
 module.exports.userService = require("./user.service");
-// module.exports.formService = require("./form.service");
+module.exports.formService = require("./form.service");
 // module.exports.submissionService = require("./submission.service");
