f436377f85e6f8cc79a2ecc483127ac934c19d7c
Tev <tevondavisjc@gmail.com>
2021-05-15 01:37:14 -0500

    import and export form route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const authRoute = require("./auth.route");
 const userRoute = require("./user.route");
-// const formRoute = require('./form.route');
+const formRoute = require("./form.route");
 // const submissionRoute = require('./submission.route');
 
 const router = express.Router();
 
 router.use("/auth", authRoute);
 router.use("/user", userRoute);
-// router.use('/forms', formRoute);
+router.use("/form", formRoute);
 // router.use('/submissions', submissionRoute);
 
 module.exports = router;
