13fcdda9f33a14c32301beca6bba323ae334fcff
Tev <tevondavisjc@gmail.com>
2021-06-01 02:21:46 -0500

    imports for submission

--- src/services/index.js
+++ src/services/index.js
@@ -3,4 +3,4 @@ module.exports.emailService = require("./email.service");
 module.exports.tokenService = require("./token.service");
 module.exports.userService = require("./user.service");
 module.exports.formService = require("./form.service");
-// module.exports.submissionService = require("./submission.service");
+module.exports.submissionService = require("./submission.service");
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,3 +1,4 @@
 module.exports.authValidation = require("./auth.validation");
 module.exports.userValidation = require("./user.validation");
 module.exports.formValidation = require("./form.validation");
+module.exports.submissionValidation = require("./submission.validation");
